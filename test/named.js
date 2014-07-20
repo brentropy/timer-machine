1f4461b6627eadfde6cdb24d72308ca8873ba9b3
Brent Burgoyne <brent47@gmail.com>
2014-07-20 10:53:43 -0600

    Added tests for named timer static methods

---
+++ test/named.js
@@ -0,0 +1,44 @@
+var should = require('should')
+var Timer  = require('../lib/timer')
+
+describe('The Timer function', function () {
+
+  var foo
+
+  before(function () {
+    foo = Timer.get('foo')
+  })
+
+  describe('get static method', function () {
+
+    it('should create a new Timer if it does not exist', function () {
+      Timer.get('bar')
+        .should.be.an.instanceof(Timer)
+        .and.should.not.equal(foo)
+      Timer.destroy('bar')
+    })
+
+    it('should return a reference to an existing named timer', function () {
+      Timer.get('foo').should.equal(foo)
+    })
+
+  })
+
+  describe('destroy static method', function () {
+
+    it('should destroy a named timer', function () {
+      Timer.destroy('foo')
+      Timer.get('foo').should.not.equal(foo)
+    })
+
+    it('should return true if the timer existed', function () {
+      Timer.destroy('foo').should.be.true
+    })
+
+    it('should return false if the timer did not exist', function () {
+      Timer.destroy('bar').should.be.false
+    })
+
+  })
+
+})
